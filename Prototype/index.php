cc8d9ee052dbfcc2b2f8d4e17b968e1d937e891b
Mosaal <AlexandreMoreira@MBPdeAlexandre.lan>
2016-03-07 20:45:43 +0000

    Template basico do LogIn e SignUp

---
+++ Prototype/index.php
@@ -0,0 +1,79 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+
+	<title>Index</title>
+
+	<!-- Bootstrap -->
+	<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+	<link rel="stylesheet" type="text/css" href="index.css">
+
+	<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+	<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+	<!--[if lt IE 9]>
+		<script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+		<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+	<![endif]-->
+</head>
+<body>
+	<div class="container row">
+		<div class="col-sm-6">
+			<form class="form-horizontal" role="form">
+				<div class="form-group">
+					<label class="control-label col-sm-2" for="username">Username:</label>
+					<div class="col-sm-10">
+						<input id="username" class="form-control" type="text" placeholder="enter username"></input>
+					</div>
+				</div>
+				<div class="form-group">
+					<label class="control-label col-sm-2" for="password">Password:</label>
+					<div class="col-sm-10">
+						<input id="password" class="form-control" type="password" placeholder="enter password"></input>
+					</div>
+				</div>
+				<div class="form-group">
+					<div class="col-sm-offset-2 col-sm-10">
+						<input class="btn btn-default" type="submit" value="Log In"></input>
+					</div>
+				</div>
+			</form>
+		</div> <!-- log in -->
+		<div class="col-sm-6">
+			<form class="form-horizontal" role="form">
+				<div class="form-group">
+					<label class="control-label col-sm-2" for="username">Username:</label>
+					<div class="col-sm-10">
+						<input id="username" class="form-control" type="text" placeholder="enter username"></input>
+					</div>
+				</div>
+				<div class="form-group">
+					<label class="control-label col-sm-2" for="email">Email:</label>
+					<div class="col-sm-10">
+						<input id="email" class="form-control" type="email" placeholder="enter email"></input>
+					</div>
+				</div>
+				<div class="form-group">
+					<label class="control-label col-sm-2" for="password">Password:</label>
+					<div class="col-sm-10">
+						<input id="password" class="form-control" type="password" placeholder="enter password"></input>
+					</div>
+				</div>
+				<div class="form-group">
+					<div class="col-sm-offset-2 col-sm-10">
+						<input class="btn btn-default" type="submit" value="Sign Up"></input>
+					</div>
+				</div>
+			</form>
+		</div> <!-- sign up -->
+	</div> <!-- container -->
+
+	<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+	<script src="jquery-1.12.1.min.js"></script>
+	<!-- Include all compiled plugins (below), or include individual files as needed -->
+	<script src="js/bootstrap.min.js"></script>
+</body>
+</html>
