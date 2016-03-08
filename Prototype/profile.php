eca3ee6a935a975aa11f5168ec710f4d12f61731
potterHub <mouraviny20004@hotmail.com>
2016-03-08 01:35:41 +0000

    start profile page only with resizable navbar on top

---
+++ Prototype/profile.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+
+	<title>Profile</title>
+
+	<!-- Bootstrap -->
+	<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+
+	<link rel="stylesheet" type="text/css" href="profile.css">
+
+
+	<!-- jquery -->
+	<script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
+
+	<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+	<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+	<!--[if lt IE 9]>
+		<script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+		<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+	<![endif]-->
+</head>
+	<body>
+		<!-- top nav bar using boot strap -->
+		<nav class="navbar navbar-inverse navbar-static-top" role="navigation">
+		  <div class="container-fluid">
+		    <div class="navbar-header">
+				<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+				</button>
+		      <a class="navbar-brand" href="#">Home</a>
+		    </div>
+		    <div id="navbar" class="navbar-collapse collapse">
+			    <ul class="nav navbar-nav">
+			    	<li><a href="#"><span class="glyphicon glyphicon-search"> Search</a></li>
+					<li><a href="#"><span class="glyphicon glyphicon-user"> Profile</a></li>
+			    </ul>
+			    <ul class="nav navbar-nav navbar-right">
+			      <li><a href="#"><span class="glyphicon glyphicon-log-out"></span> LogOut</a></li>
+			    </ul>
+			</div>
+		  </div>
+		</nav>
+
+		<h1> Profile </h1>
+
+
+
+		<!-- boot strap java script (in the end to load faster) -->
+		<script type="text/javascript" src="js/bootstrap.min.js"></script>
+	</body>
+</html>
