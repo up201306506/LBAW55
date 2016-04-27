42529cb8912fa1f0b78f5a186197799fbf47c4d1
Tropical Drink <emailfalsonumero4@gmail.com>
2016-04-27 16:38:39 +0100

    :tangerine: Okay vou repor isto e vou fazer numa branch
    https://gnomo.fe.up.pt/~lbaw1555/LBAW55/proto/login_signup.php

--- proto/config/init.php
+++ proto/config/init.php
@@ -7,13 +7,13 @@
   $BASE_DIR = '/opt/lbaw/lbaw1555/public_html/LBAW55/'; //FIXME
   $BASE_URL = '/~lbaw1555/src/'; //FIXME
 
-  /*
+
   $conn = new PDO('pgsql:host=dbm;dbname=lbaw1555', 'lbaw1555', 'VK15D1P4'); //FIXME
   $conn->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
   $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
   $conn->exec('SET SCHEMA \'public\''); //FIXME?
-  */
+  
   
   include_once($BASE_DIR . 'lib/smarty/Smarty.class.php');
   
--- proto/login_signup.php
+++ proto/login_signup.php
@@ -1,6 +1,79 @@
-<?php
-	include_once('../../config/init.php');
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Log In/Sign Up</title>
 
-	
-	$smarty->display('login_signup.tpl');
-?>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+
+	<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+	<script src="javascript/jquery-1.12.1.min.js"></script>
+	<!-- Bootstrap -->
+	<link rel="stylesheet" type="text/css" href="css/Bootstrap/css/bootstrap.min.css">
+	<link rel="stylesheet" type="text/css" href="css/login_signup.css">
+
+	<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+	<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+	<!--[if lt IE 9]>
+		<script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+		<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+	<![endif]-->
+</head>
+<body>
+	<h1 class="title">eldooM</h1>
+	<div class="container-parent">
+		<div class="container">
+			<div class="col-sm-offset-3 col-sm-6">
+				<ul class="tabs">
+					<li class="tab-link current" data-tab="tab-1">Log In</li>
+					<li class="tab-link" data-tab="tab-2">Sign Up</li>
+				</ul>
+				<div class="tab-content current" id="tab-1">
+					<div id="login-form">
+						<form class="form-horizontal" action="profile/profile.php" method="post" role="form">
+							<div class="form-group">
+								<input id="username" class="form-control" type="text" placeholder="USERNAME"></input>
+							</div>
+							<div class="form-group">
+								<input id="password" class="form-control" type="password" placeholder="PASSWORD"></input>
+							</div>
+							<div class="form-group">
+								<input class="btn btn-default" type="submit" value="Log In"></input>
+							</div>
+						</form>
+					</div>
+				</div>
+				<div class="tab-content" id="tab-2">
+					<div id="signup-form">
+						<form class="form-horizontal" action="profile/profile.php" method="post" role="form">
+							<div class="form-group">
+								<input id="username" class="form-control" type="text" placeholder="USERNAME"></input>
+							</div>
+							<div class="form-group">
+								<input id="email" class="form-control" type="text" placeholder="EMAIL"></input>
+							</div>
+							<div class="form-group">
+								<input id="password" class="form-control" type="password" placeholder="PASSWORD"></input>
+							</div>
+							<div class="form-group radio-group">
+								<label class="radio-inline"><input type="radio" name="optradio"/>Professor</label>
+								<label class="radio-inline"><input type="radio" name="optradio"/>Student</label>
+							</div>
+							<div class="form-group">
+								<input class="btn btn-default" type="submit" value="Sign Up"></input>
+							</div>
+						</form>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+
+	<!-- Include file to make tabs work -->
+	<script src="javascript/tabs.js"></script>
+	<!-- Include all compiled plugins (below), or include individual files as needed -->
+	<script src="css/Bootstrap/js/bootstrap.min.js"></script>
+</body>
+</html>
