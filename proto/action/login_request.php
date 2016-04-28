773b7d17e7df018ed81ecc371249338fa52a5e19
potterHub <mouraviny20004@hotmail.com>
2016-04-28 23:15:57 +0100

    login testing

--- proto/action/login_request.php
+++ proto/action/login_request.php
@@ -12,7 +12,13 @@
 );
 	*/	
 
-include_once('../config/init.php');	
+//include_once('../config/init.php');	
+
+  $conn = new PDO('pgsql:host=dbm;dbname=lbaw1555', 'lbaw1555', 'VK15D1P4'); //FIXME
+  $conn->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+  $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+  $conn->exec('SET SCHEMA \'public\''); //FIXME?
 
 /*
 	function isLoginCorrect($username, $password) {
