56d880c9d0efa9d9f3ce0f7d6bcc117342e73601
potterHub <mouraviny20004@hotmail.com>
2016-04-28 22:21:13 +0100

    testing again wrong path

---
+++ proto/action/create_user.php
@@ -0,0 +1,8 @@
+<?php
+
+	$username = $_POST['username'];
+	$password = $_POST['password'];
+
+	echo '<script type="text/javascript">alert(" user name: '.$username.',password: '.$password.'"); </script>';
+
+?>
