8d4b749a96f9cd275ba9bd0c7184b531c2cb5383
potterHub <mouraviny20004@hotmail.com>
2016-04-29 00:58:05 +0100

    login testing

--- proto/action/login_request.php
+++ proto/action/login_request.php
@@ -18,8 +18,8 @@
 	    global $conn;
 	    $stmt = $conn->prepare("SELECT * 
 	                            FROM users 
-	                            WHERE username = '?' AND password = ?");// needs the isActive contrain
-	    $stmt->execute(array($username,sha1($password)));// sha1($password)
+	                            WHERE username = ? AND password = ?");// needs the isActive contrain
+	    $stmt->execute(array($username, $password));// sha1($password)
 	    return $stmt->fetch() == true;
  	}
 
