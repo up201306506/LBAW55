31f95a01fc8c079c1a6a20520aca0304501f6075
potterHub <mouraviny20004@hotmail.com>
2016-04-28 23:19:21 +0100

    login testing

--- proto/action/login_request.php
+++ proto/action/login_request.php
@@ -12,13 +12,7 @@
 );
 	*/	
 
-//include_once('../config/init.php');	
-
-  $conn = new PDO('pgsql:host=dbm;dbname=lbaw1555', 'lbaw1555', 'VK15D1P4'); //FIXME
-  $conn->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
-  $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-
-  $conn->exec('SET SCHEMA \'public\''); //FIXME?
+	include_once('../config/init.php');	
 
 /*
 	function isLoginCorrect($username, $password) {
@@ -56,7 +50,7 @@
 	  while ($row = $result->fetch(PDO::FETCH_ASSOC))
 	  {
 
-	   echo '<script type="text/javascript">alert(" username: '.$row['username'].' "); </script>';
+	   echo '<script type="text/javascript">alert(" username: '.json_encode($row).' "); </script>';
 	  }
 
 ?>
