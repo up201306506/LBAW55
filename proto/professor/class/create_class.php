18b827de07ee7f6d52c578a8f1e442e25efb40d9
Mosaal <AlexandreMoreira@wifi-interno.fe.up.pt>
2016-04-29 08:39:49 +0100

    Experimenting

--- proto/professor/class/create_class.php
+++ proto/professor/class/create_class.php
@@ -1,34 +1,13 @@
-<!DOCTYPE html>
-<html>
-<head>
-	<meta charset="utf-8">
-	<meta http-equiv="X-UA-Compatible" content="IE=edge">
-	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+<?php
 
-	<title>Create Class</title>
+	/*This summons the database and smarty initializer */
+	include_once('config/init.php');
 
-	<!-- Bootstrap -->
-	<link rel="stylesheet" type="text/css" href="../../css/Bootstrap/css/bootstrap.min.css">
-	<link rel="stylesheet" type="text/css" href="../../css/create_exam.css">
-	<link rel="stylesheet" type="text/css" href="../../css/navbar.css">
+	/*Other PHP actions should go here*/
+	$pagename = 'create_class';
+	$smarty->assign('pagename', $pagename);
+	
+	/*This summons the smarty template*/
+	$smarty->display('create_class.tpl');
 
-	<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
-	<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-	<!--[if lt IE 9]>
-		<script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
-		<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-	<![endif]-->
-</head>
-<body>
-	<?php include '../../navbar.php'; ?>
-	<div class="container">
-		
-	</div>
-
-	<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
-	<script src="../../javascript/jquery-1.12.1.min.js"></script>
-	<!-- Include all compiled plugins (below), or include individual files as needed -->
-	<script src="../../css/Bootstrap/js/bootstrap.min.js"></script>
-</body>
-</html>
+?>
---
+++ proto/templates/professor/class/create_class.tpl
@@ -0,0 +1,8 @@
+{include file='common/header.tpl'}
+
+	<?php include '../../navbar.php'; ?>
+	<div class="container">
+		<h1>lol</h1>
+	</div>
+
+{include file='common/footer.tpl'}
