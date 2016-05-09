1b4e7dbf71866aa36b63cfe98a7ee898017a6328
Tropical Drink <emailfalsonumero4@gmail.com>
2016-05-09 16:10:57 +0100

    mnrkrnvkrn

---
+++ proto/profile/profileTemplate.php
@@ -0,0 +1,21 @@
+<?php
+
+	/*This summons the database and smarty initializer */
+	include_once('../config/init.php');
+
+	/*Other PHP actions should go here*/
+	$pagename = 'Profile';
+	$smarty->assign('pagename', $pagename);
+	
+	$session_username = $_SESSION['username'];
+	$smarty->assign('session_username', $session_username);
+	$session_name = $_SESSION['name'];
+	$smarty->assign('session_name', $session_name);
+	$session_email = $_SESSION['email'];
+	$smarty->assign('session_email', $session_email);
+	
+	
+	/*This summons the smarty template*/
+	$smarty->display('profile.tpl');
+
+?>
