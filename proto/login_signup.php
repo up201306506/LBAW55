72c388ede4284e840d1be3293d947be09651ee1c
Tropical Drink <emailfalsonumero4@gmail.com>
2016-05-10 01:52:58 +0100

    :tropical_drink:

--- proto/login_signup.php
+++ proto/login_signup.php
@@ -8,10 +8,10 @@
 
 	$smarty->assign('bootstrap', "css/Bootstrap/css/bootstrap.min.css");
 	$smarty->assign('csspage', "css/login_signup.css");
+	$smarty->assign('cssnavbar', "css/navbar.css");
 
 	$smarty->assign('script_boot', "/css/Bootstrap/js/bootstrap.min.js");
-	$smarty->assign('script_jquery', "/javascript/jquery-1.12.1.min.js");
-	$smarty->assign('script_tabs', "/javascript/tabs.js");
+	$smarty->assign('script_tabs', "javascript/tabs.js");
 	
 	/*This summons the smarty template*/
 	$smarty->display('login_signup.tpl');
