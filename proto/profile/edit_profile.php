14ecae309bb55a6cc0f0e0e68cdc2e0eecccf505
potterHub <mouraviny20004@hotmail.com>
2016-05-12 23:27:22 +0100

    udpate edit profile

--- proto/profile/edit_profile.php
+++ proto/profile/edit_profile.php
@@ -26,38 +26,38 @@
 		<h1 id="hum" class="col-sm-offset-3 col-sm-6">Edit Profile</h1>
 		<div id="edit-box" class="col-sm-offset-3 col-sm-6">
 			<div id="edit-name-box">
-				<form action="profile.php" method="post" role="form">
+				<form action="edit_profile_action.php?update=name" method="post" role="form">
 					<div class="form-group">
 						<label for="name">Name:</label>
-						<input id="name" class="form-control" type="text"></input>
+						<input id="name" name="name" class="form-control" type="text"></input>
 					</div>
 					<input id="edit-name" class="btn btn-primary" type="submit" value="Submit new Name"></input>
 				</form> <!-- Edit name -->
 			</div>
 			<div id="edit-email-box">
-				<form action="profile.php" method="post" role="form">
+				<form action="edit_profile_action.php?update=email" method="post" role="form">
 					<div class="form-group">
 						<label for="email">Email:</label>
-						<input id="email" class="form-control" type="text"></input>
+						<input id="email" name="email" class="form-control" type="text"></input>
 					</div>
 					<input id="edit-email" class="btn btn-primary" type="submit" value="Submit new Email"></input>
 				</form> <!-- Edit email -->
 			</div>
 			<div id="edit-password-box">
-				<form action="profile.php" method="post" role="form">
+				<form action="edit_profile_action.php?update=password" method="post" role="form">
 					<div class="form-group">
 						<label for="password">Password:</label>
-						<input id="password" class="form-control" type="password"></input>
+						<input id="password" name="password" class="form-control" type="password"></input>
 					</div>
 					<div class="form-group">
 						<label for="re-password">Confirm Password:</label>
-						<input id="re-password" class="form-control" type="password"></input>
+						<input id="re-password" name="re-password" class="form-control" type="password"></input>
 					</div>
 					<input id="edit-password" class="btn btn-primary" type="submit" value="Submit new Password"></input>
 				</form> <!-- Edit password -->
 			</div>
 			<div id="edit-description-box">
-				<form action="profile.php" method="post" role="form">
+				<form action="edit_profile_action.php?update=description" method="post" role="form">
 					<div class="form-group">
 						<label for="description">Description:</label>
 						<textarea id="description" class="form-control" rows="6"></textarea>
@@ -66,7 +66,7 @@
 				</form> <!-- Edit description -->
 			</div>
 			<div id="edit-profile-pic-box">
-				<form action="profile.php" method="post" role="form">
+				<form action="edit_profile_action.php?update=picture" method="post" role="form">
 					<div class="form-group">
 						<label for="upload_img">Profile Picture:</label>
 						<input id="upload_img" type="file" accept="image/*"></input>
---
+++ proto/profile/edit_profile_action.php
@@ -0,0 +1,47 @@
+<?php
+	include_once('../config/init.php');	
+	include_once('../database/user_functions.php');
+
+switch ($_GET['update']) {
+		case 'name':
+			$username = $_POST['inputUserNameTextField'];
+			echo '<p>'.$username.'</p>';
+
+		  break;
+
+		case 'email':
+			$email = $_POST['email'];
+			echo '<p>'.$email.'</p>';
+		  break;
+		case 'password':
+			$password = $_POST['password'];
+			$re_password = $_POST['re-password'];
+			
+			echo '<p>'.$password.'</p><br>';
+			echo '<p>'.$re_password.'</p>';
+
+		  break;
+
+		case 'description':
+		 	$description = $_POST['description'];
+
+		 	echo '<p>'.$description.'</p>';
+		 	break;
+		case 'picture':
+			$picture = $_POST['upload_img'];
+
+			echo '<p>'.$picture.'</p>';
+		 	break;
+
+
+		default:
+		  break;
+	}
+
+
+
+
+
+
+
+?>
