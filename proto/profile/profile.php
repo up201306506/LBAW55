68205ab5ee0a8b989be15ffb5e28eb0943ca7efe
Mosaal <AlexandreMoreira@MBPdeAlexandre.lan>
2016-05-24 23:24:01 +0100

    Replacing old files

--- proto/profile/profile.php
+++ proto/profile/profile.php
@@ -1,5 +1,4 @@
 <?php
-
 	/*This summons the database and smarty initializer */
 	include_once('../config/init.php');
 	include_once('../database/user_functions.php');
--- proto/templates/professor/class/create_class.tpl
+++
@@ -1,8 +0,0 @@
-{include file='common/header.tpl'}
-
-	{$navbar}
-	<div class="container">
-		<h1>TESTE</h1>
-	</div>
-
-{include file='common/footer.tpl'}
