513df56400151bc61799e578a9a026e2c538e617
Mosaal <AlexandreMoreira@MacBook-Pro-de-Alexandre.local>
2016-05-25 19:26:23 +0100

    Fix

--- proto/javascript/create_question.js
+++ proto/javascript/create_question.js
@@ -1,4 +1,4 @@
-var answer = "<div class=\"form-group\"><label for=\"answer\">Answer#:</label><textarea id=\"answer\" class=\"form-control\" rows=\"5\"></textarea></div>";
+var answer = "<div class=\"form-group answer\"><label for=\"answer\">Answer#:</label><textarea id=\"answer\" class=\"form-control\" rows=\"5\"></textarea></div>";
 
 $(document).ready(function() {
 	$("#add_answer").click(function() {
