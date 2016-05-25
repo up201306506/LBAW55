a513f61207799ead90372034c90e62d9fc16dd42
Mosaal <AlexandreMoreira@MacBook-Pro-de-Alexandre.local>
2016-05-25 19:04:18 +0100

    fix

--- proto/professor/question/create_question.php
+++ proto/professor/question/create_question.php
@@ -1,7 +1,7 @@
 <?php
 
 	/*This summons the database and smarty initializer */
-	include_once('../config/init.php');
+	include_once('../../config/init.php');
 
 	/*Other PHP actions should go here*/
 	$pagename = 'Create Question';
