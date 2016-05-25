241cd0fa0dff181a4c48bb83d3095aef6e42ee83
Mosaal <AlexandreMoreira@MacBook-Pro-de-Alexandre.local>
2016-05-25 18:48:07 +0100

    Fixed

--- proto/professor/class/create_class.php
+++ proto/professor/class/create_class.php
@@ -7,12 +7,12 @@
 	$pagename = 'Create Class';
 	$smarty->assign('pagename', $pagename);
 
-	$smarty->assign('bootstrap', "../css/Bootstrap/css/bootstrap.min.css");
-	$smarty->assign('csspage', "../css/create_class.css");
-	$smarty->assign('cssnavbar', "../css/navbar.css");
+	$smarty->assign('bootstrap', "../../css/Bootstrap/css/bootstrap.min.css");
+	$smarty->assign('csspage', "../../css/create_class.css");
+	$smarty->assign('cssnavbar', "../../css/navbar.css");
 
-	$smarty->assign('script_boot', "../css/Bootstrap/js/bootstrap.min.js");
-	$smarty->assign('script_jquery', "../javascript/jquery-1.12.1.min.js");
+	$smarty->assign('script_boot', "../../css/Bootstrap/js/bootstrap.min.js");
+	$smarty->assign('script_jquery', "../../javascript/jquery-1.12.1.min.js");
 	
 	/*This summons the smarty template*/
 	$smarty->display('professor/class/create_class.tpl');
