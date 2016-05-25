a0834a5f4b1db60d749a7b763e8c02a6770528b1
Mosaal <AlexandreMoreira@MacBook-Pro-de-Alexandre.local>
2016-05-26 00:09:59 +0100

    i give up

--- proto/javascript/create_exam.js
+++ proto/javascript/create_exam.js
@@ -1,14 +1,14 @@
 $(document).ready(function() {
-	var plusHtml = $(".question-button").html();
+	// var plusHtml = $(".question-button").html();
 
-	$(".question-button").click(function() {
-		var innerHtml = $(this).html();
-		if (innerHtml === plusHtml) {
-			$(this).html("<span class=\"glyphicon glyphicon-minus\"></span>");
-			$("#" + $(this).attr("panel-button")).removeClass("panel-default").addClass("panel-success");
-		} else {
-			$(this).html("<span class=\"glyphicon glyphicon-plus\"></span>");
-			$("#" + $(this).attr("panel-button")).removeClass("panel-success").addClass("panel-default");
-		}
-	});
+	// $(".question-button").click(function() {
+	// 	var innerHtml = $(this).html();
+	// 	if (innerHtml === plusHtml) {
+	// 		$(this).html("<span class=\"glyphicon glyphicon-minus\"></span>");
+	// 		$("#" + $(this).attr("panel-button")).removeClass("panel-default").addClass("panel-success");
+	// 	} else {
+	// 		$(this).html("<span class=\"glyphicon glyphicon-plus\"></span>");
+	// 		$("#" + $(this).attr("panel-button")).removeClass("panel-success").addClass("panel-default");
+	// 	}
+	// });
 });
