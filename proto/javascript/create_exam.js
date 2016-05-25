62219f7c69231aa66bd2e7e85d2797ae5888c012
Mosaal <AlexandreMoreira@MacBook-Pro-de-Alexandre.local>
2016-05-25 23:54:10 +0100

    meh

--- proto/javascript/create_exam.js
+++ proto/javascript/create_exam.js
@@ -1,7 +1,8 @@
 $(document).ready(function() {
 	$(".question-button").click(function() {
-		if ($(this).html() == '<span class="glyphicon glyphicon-plus"></span>') {
-			console.log("LEL WUT");
+		var innerHtml = $(this).html();
+		if (innerHtml == "<span class=\"glyphicon glyphicon-plus\"></span>") {
+			console.log(innerHtml);
 		}
 		// $(this).html("<span class=\"glyphicon glyphicon-minus\"></span>");
 		// $("#" + $(this).attr("panel-button")).removeClass("panel-default").addClass("panel-success");
