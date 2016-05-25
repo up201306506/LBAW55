a8549b102f1cec0bc8609ca137d0b09ea55efaa0
Mosaal <AlexandreMoreira@MacBook-Pro-de-Alexandre.local>
2016-05-25 23:52:06 +0100

    ¯\_(ツ)_/¯

--- proto/javascript/create_exam.js
+++ proto/javascript/create_exam.js
@@ -1,9 +1,9 @@
 $(document).ready(function() {
 	$(".question-button").click(function() {
-		//if ($(this).html() == "<span class=\"glyphicon glyphicon-plus\"></span>") {
-			console.log($(this).html());
-		//}
+		if ($(this).html() == '<span class="glyphicon glyphicon-plus"></span>') {
+			console.log("LEL WUT");
+		}
 		// $(this).html("<span class=\"glyphicon glyphicon-minus\"></span>");
-		$("#" + $(this).attr("panel-button")).removeClass("panel-default").addClass("panel-success");
+		// $("#" + $(this).attr("panel-button")).removeClass("panel-default").addClass("panel-success");
 	});
 });
