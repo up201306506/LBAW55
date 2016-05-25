a4b442ce3be02e3040a4ccaa866427649119825e
Mosaal <AlexandreMoreira@MacBook-Pro-de-Alexandre.local>
2016-05-25 19:29:39 +0100

    Meh

--- proto/javascript/create_question.js
+++ proto/javascript/create_question.js
@@ -1,8 +1,6 @@
-var answer = "<div class=\"form-group answer\"><label for=\"answer\">Answer#:</label><textarea id=\"answer\" class=\"form-control\" rows=\"5\"></textarea></div>";
-
 $(document).ready(function() {
 	$("#add_answer").click(function() {
-		console.log("There are " + $(".answer").length);
+		var answer = "<div class=\"form-group answer\"><label for=\"answer\">Answer#" + ($(".answer").length + 1) + ":</label><textarea id=\"answer\" class=\"form-control\" rows=\"5\"></textarea></div>";
 		$(".answer").last().after(answer);
 	});
 });
