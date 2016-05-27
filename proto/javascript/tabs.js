cff74ca41a75aa13956aeb295c6d2e45772091b6
Mosaal <AlexandreMoreira@MacBook-Pro-de-Alexandre.local>
2016-05-27 01:55:37 +0100

    Small fix

--- proto/javascript/tabs.js
+++ proto/javascript/tabs.js
@@ -1,4 +1,20 @@
 $(document).ready(function() {
+	var WIDTH = 500;
+
+	if($(window).width() < 768) {
+		$("#form-box").width($(window).width());
+	} else {
+		$("#form-box").width(WIDTH);
+	}
+
+	$(window).resize(function() {
+		if($(window).width() < 768) {
+			$("#form-box").width($(window).width());
+		} else {
+			$("#form-box").width(WIDTH);
+		}
+	});
+
 	$("#tabs li span").click(function() {
 		var tab_id = $(this).parent().attr("data-tab");
 
