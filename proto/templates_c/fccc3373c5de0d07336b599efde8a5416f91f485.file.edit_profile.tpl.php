5d00292ea23c47ceba6d0185d9c8bca3276c8f9c
Mosaal <AlexandreMoreira@MacBook-Pro-de-Alexandre.local>
2016-05-29 04:07:33 +0100

    Meh

--- proto/templates_c/fccc3373c5de0d07336b599efde8a5416f91f485.file.edit_profile.tpl.php
+++ proto/templates_c/fccc3373c5de0d07336b599efde8a5416f91f485.file.edit_profile.tpl.php
@@ -1,4 +1,4 @@
-<?php /* Smarty version Smarty-3.1.15, created on 2016-05-29 05:04:32
+<?php /* Smarty version Smarty-3.1.15, created on 2016-05-29 05:06:34
          compiled from "/Applications/MAMP/htdocs/LBAW55/proto/templates/profile/edit_profile.tpl" */ ?>
 <?php /*%%SmartyHeaderCode:142052089857485514859639-21585156%%*/if(!defined('SMARTY_DIR')) exit('no direct access allowed');
 $_valid = $_smarty_tpl->decodeProperties(array (
@@ -7,7 +7,7 @@ $_valid = $_smarty_tpl->decodeProperties(array (
     'fccc3373c5de0d07336b599efde8a5416f91f485' => 
     array (
       0 => '/Applications/MAMP/htdocs/LBAW55/proto/templates/profile/edit_profile.tpl',
-      1 => 1464491057,
+      1 => 1464491170,
       2 => 'file',
     ),
   ),
@@ -17,6 +17,10 @@ $_valid = $_smarty_tpl->decodeProperties(array (
   ),
   'version' => 'Smarty-3.1.15',
   'unifunc' => 'content_5748551493c9e2_07711416',
+  'variables' => 
+  array (
+    'BASE_URL' => 0,
+  ),
   'has_nocache_code' => false,
 ),false); /*/%%SmartyHeaderCode%%*/?>
 <?php if ($_valid && !is_callable('content_5748551493c9e2_07711416')) {function content_5748551493c9e2_07711416($_smarty_tpl) {?><?php echo $_smarty_tpl->getSubTemplate ('common/header.tpl', $_smarty_tpl->cache_id, $_smarty_tpl->compile_id, 0, null, array(), 0);?>
@@ -29,7 +33,8 @@ $_valid = $_smarty_tpl->decodeProperties(array (
 		<div class="col-sm-offset-3 col-sm-6">
 			<h1>Edit Profile</h1>
 			<div class="well">
-				<form role="form">
+				<form action="<?php echo ($_smarty_tpl->tpl_vars['BASE_URL']->value).('action/edit_profile_action.php');?>
+" method="post" role="form">
 					<div class="form-group">
 						<label for="name">Name:</label>
 						<input id="name" name="name" class="form-control" type="text"></input>
