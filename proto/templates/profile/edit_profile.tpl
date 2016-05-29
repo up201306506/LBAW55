3013861a7edee250f8c07a53cf03b21094dc6a1c
Mosaal <AlexandreMoreira@MacBook-Pro-de-Alexandre.local>
2016-05-29 04:04:24 +0100

    Removed

--- proto/templates/profile/edit_profile.tpl
+++ proto/templates/profile/edit_profile.tpl
@@ -59,6 +59,5 @@
 </div> <!-- wrapper -->
 
 {include file='javascript_plugins/jquery.tpl'}
-{include file='javascript_plugins/script.tpl'}
 {include file='javascript_plugins/bootstrap.tpl'}
 {include file='common/footer.tpl'}
--- proto/templates_c/fccc3373c5de0d07336b599efde8a5416f91f485.file.edit_profile.tpl.php
+++ proto/templates_c/fccc3373c5de0d07336b599efde8a5416f91f485.file.edit_profile.tpl.php
@@ -1,4 +1,4 @@
-<?php /* Smarty version Smarty-3.1.15, created on 2016-05-29 05:00:02
+<?php /* Smarty version Smarty-3.1.15, created on 2016-05-29 05:03:50
          compiled from "/Applications/MAMP/htdocs/LBAW55/proto/templates/profile/edit_profile.tpl" */ ?>
 <?php /*%%SmartyHeaderCode:142052089857485514859639-21585156%%*/if(!defined('SMARTY_DIR')) exit('no direct access allowed');
 $_valid = $_smarty_tpl->decodeProperties(array (
@@ -7,7 +7,7 @@ $_valid = $_smarty_tpl->decodeProperties(array (
     'fccc3373c5de0d07336b599efde8a5416f91f485' => 
     array (
       0 => '/Applications/MAMP/htdocs/LBAW55/proto/templates/profile/edit_profile.tpl',
-      1 => 1464490367,
+      1 => 1464490960,
       2 => 'file',
     ),
   ),
